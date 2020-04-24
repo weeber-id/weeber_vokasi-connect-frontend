1b904e7c56adf68ac57fea28b423e79fe843b58d
zhorifmaulana <zhorif.maulana@ui.ac.id>
2020-04-24 11:44:18 +0700

    test branch

--- src/pages/triumph_of_spartan/triumph_of_spartan_page.jsx
+++ src/pages/triumph_of_spartan/triumph_of_spartan_page.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
+import CardList from '../../components/cards/cardlist';
+import Documentation from '../../components/documentation';
 
+const Triumph = () => {
+    return(
+        <div>
+            <CardList/>
+            <Documentation/>
+        </div>
+    );
+}
