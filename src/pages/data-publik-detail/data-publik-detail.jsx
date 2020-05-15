7ad4be55d960192e0637df676b3da58fce354c6f
Azhar Ali <azharalifauzi@gmail.com>
2020-05-16 00:21:22 +0700

    adding mobile responsive feautre

--- src/pages/data-publik-detail/data-publik-detail.jsx
+++ src/pages/data-publik-detail/data-publik-detail.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './data-publik-detail.styles.scss';
 
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import Button from '../../components/button/button';
 
 const DataPublikDetail = () => {
+  const { id } = useParams();
+
+  const name = id.split('-').join(' ').toUpperCase();
   return (
     <article className="data-publik-detail-page">
       <Header color="green" />
@@ -14,22 +17,36 @@ const DataPublikDetail = () => {
         <section className="fake-header">
           <div className="button-header-container">
             <Link to="/data-publik/data-riset">
-              <Button>
+              <Button
+                className={`btn--up-page ${
+                  id === 'data-riset' ? 'active' : ''
+                }`}
+              >
                 <h3 className="text">Data Riset</h3>
               </Button>
             </Link>
             <Link to="/data-publik/buletin">
-              <Button>
+              <Button
+                className={`btn--up-page ${id === 'buletin' ? 'active' : ''}`}
+              >
                 <h3 className="text">Buletin</h3>
               </Button>
             </Link>
             <Link to="/data-publik/kajian-strategis">
-              <Button>
+              <Button
+                className={`btn--up-page ${
+                  id === 'kajian-strategis' ? 'active' : ''
+                }`}
+              >
                 <h3 className="text">Kajian Strategis</h3>
               </Button>
             </Link>
             <Link to="/data-publik/uu-ikm-vokasi-ui">
-              <Button>
+              <Button
+                className={`btn--up-page ${
+                  id === 'uu-ikm-vokasi-ui' ? 'active' : ''
+                }`}
+              >
                 <h3 className="text">UU IKM Vokasi UI</h3>
               </Button>
             </Link>
@@ -37,17 +54,19 @@ const DataPublikDetail = () => {
         </section>
         <section className="data-publik-table-section">
           <div className="data-publik-table-title">
-            <h1 className="heading-primary text-align-center">Data Riset</h1>
+            <h1 className="heading-primary text-align-center">{name}</h1>
           </div>
 
           <div className="data-publik-container">
             <table className="data-publik-table">
-              <tr className="data-publik-table__header">
-                <th>Judul</th>
-                <th>Pengunggah</th>
-                <th>Tanggal</th>
-                <th> </th>
-              </tr>
+              <thead>
+                <tr className="data-publik-table__header">
+                  <th>Judul</th>
+                  <th>Pengunggah</th>
+                  <th>Tanggal</th>
+                  <th></th>
+                </tr>
+              </thead>
               <tbody>
                 <tr>
                   <td>Quo Vadis Pendidikan Vokasi di Indonesia</td>
